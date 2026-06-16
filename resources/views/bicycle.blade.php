6e7f0231145e7773ba4f8d65ccd599976f4459dd
nanashi <cibicco323@gmail.com>
2026-06-16 16:00:26 +0900

    sm適応

--- resources/views/bicycle.blade.php
+++ resources/views/bicycle.blade.php
@@ -5,11 +5,12 @@
 @section('content')
 
 <section>
-    <img src="{{ asset('img/bicycle1.jpg') }}" alt="Profile Image" class="object-cover w-full h-[500px]  mb-16 px-16">
+    <img src="{{ asset('img/bicycle1.jpg') }}" alt="Profile Image" class="object-cover w-full h-[500px]  mb-16 px-16 max-sm:h-[300px]">
     <div class="flex flex-col text-left px-20">
         <h2 class="text-left text-2xl font-bold  mb-4">タイトルタイトル1</h2>
         <p class="text-left mb-16">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
     </div>
+    <p class="text-center mb-16"><a href="{{ url('index') }}">トップに戻る</a></p>
 </section>
 
 @endsection
